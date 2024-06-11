fdea7e082b06dbc110113942f2b52f16265caacd
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2024-06-11 23:08:18 +0200

    test

--- build_tests.sh
+++ build_tests.sh
@@ -6,12 +6,18 @@ cd src/tests/demo_test
 
 cargo build --release --target wasm32-unknown-unknown
 
+echo current folder `pwd`
+
+ls
+
+ls target/wasm32-unknown-unknown/release/
+
 zip target/wasm32-unknown-unknown/release/demo_test_backend.zip target/wasm32-unknown-unknown/release/demo_test_backend.wasm
 
 cd ../demo_test_upgraded
 
 cargo build --release --target wasm32-unknown-unknown
 
-zip target/wasm32-unknown-unknown/release/demo_test_backend.zip target/wasm32-unknown-unknown/release/demo_test_backend.wasm
+zip target/wasm32-unknown-unknown/release/demo_test_upgraded_backend.zip target/wasm32-unknown-unknown/release/demo_test_upgraded_backend.wasm
 
 
